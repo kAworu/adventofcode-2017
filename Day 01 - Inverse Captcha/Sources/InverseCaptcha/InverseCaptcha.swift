0d0053cb72a3fad523f7722bcbf6020c7d0db1b8
Alexandre Perrin <alex@kaworu.ch>
2017-12-01 19:29:34 +0100

    Cosmetic

--- Day 01 - Inverse Captcha/Sources/InverseCaptcha/InverseCaptcha.swift
+++ Day 01 - Inverse Captcha/Sources/InverseCaptcha/InverseCaptcha.swift
@@ -2,23 +2,23 @@ public class InverseCaptcha {
   var digits: Array<Int>
 
   public init(_ captcha: String) {
-    self.digits = captcha.map { Int(String($0))! }
+    digits = captcha.map { Int(String($0))! }
   }
 
   // NOTE: part one
   public func solve_next() -> Int {
-    return self.solve { $0 + 1 }
+    return solve { $0 + 1 }
   }
 
   // NOTE: part two
   public func solve_halfway_around() -> Int {
-    return self.solve { $0 + self.digits.count / 2 }
+    return solve { $0 + digits.count / 2 }
   }
 
   internal func solve(next: (Int) -> Int) -> Int {
-    return self.digits.indices.reduce(0, { acc, i in
-      let current = self.digits[i]
-      let match   = self.digits[next(i) % self.digits.count]
+    return digits.indices.reduce(0, { acc, i in
+      let current = digits[i]
+      let match   = digits[next(i) % digits.count]
       return acc + (current == match ? current : 0)
     })
   }
