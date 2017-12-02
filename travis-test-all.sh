600ba8f41dbe7e3c8cfbfee5a0c353ff437ea3b0
Alexandre Perrin <alex@kaworu.ch>
2017-12-02 17:35:34 +0100

    Another fix attempt for TravisCI

--- travis-test-all.sh
+++ travis-test-all.sh
@@ -1,14 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 set -e
 
 DIR=$(dirname "$0")
 DAYS=$(find "$DIR" -maxdepth 1 -type d -name 'Day [0-9]*')
-
-# see .travis.yml
-if [ -f ~/.swiftenv/init ]; then
-    source ~/.swiftenv/init
-fi
 
 echo "$DAYS" | while read DAY; do
     echo "===>" $(basename "$DAY")
