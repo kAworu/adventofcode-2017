f81b4d87580b890b68f3b4ea181d63c78478cd0e
Alexandre Perrin <alex@kaworu.ch>
2017-12-03 09:32:19 +0100

    Revert "Another fix attempt for TravisCI"
    This reverts commit 600ba8f41dbe7e3c8cfbfee5a0c353ff437ea3b0.

--- travis-test-all.sh
+++ travis-test-all.sh
@@ -1,9 +1,14 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -e
 
 DIR=$(dirname "$0")
 DAYS=$(find "$DIR" -maxdepth 1 -type d -name 'Day [0-9]*')
+
+# see .travis.yml
+if [ -f ~/.swiftenv/init ]; then
+    source ~/.swiftenv/init
+fi
 
 echo "$DAYS" | while read DAY; do
     echo "===>" $(basename "$DAY")
