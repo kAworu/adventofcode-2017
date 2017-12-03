5e1f837b8f1ed154845a6d9025adb982a0687918
Alexandre Perrin <alex@kaworu.ch>
2017-12-03 09:33:32 +0100

    Cm'on TravisCI

--- travis-test-all.sh
+++ travis-test-all.sh
@@ -5,12 +5,12 @@ set -e
 DIR=$(dirname "$0")
 DAYS=$(find "$DIR" -maxdepth 1 -type d -name 'Day [0-9]*')
 
-# see .travis.yml
-if [ -f ~/.swiftenv/init ]; then
-    source ~/.swiftenv/init
-fi
-
 echo "$DAYS" | while read DAY; do
     echo "===>" $(basename "$DAY")
+    # see .travis.yml
+    if [ -f ~/.swiftenv/init ]; then
+        cat ~/.swiftenv/init
+        . ~/.swiftenv/init
+    fi
     (cd "$DAY" && swift test --verbose)
 done
