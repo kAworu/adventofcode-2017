449e4e563a7394740fa3db63f3bbb47e33a733cb
Alexandre Perrin <alex@kaworu.ch>
2017-12-06 18:29:46 +0100

    Make TravisCI test the days in order.

--- travis-test-all.sh
+++ travis-test-all.sh
@@ -3,7 +3,7 @@
 set -e
 
 DIR=$(dirname "$0")
-DAYS=$(find "$DIR" -maxdepth 1 -type d -name 'Day [0-9]*')
+DAYS=$(find "$DIR" -maxdepth 1 -type d -name 'Day [0-9]*' | sort)
 
 echo "$DAYS" | while read DAY; do
     echo "===>" $(basename "$DAY")
