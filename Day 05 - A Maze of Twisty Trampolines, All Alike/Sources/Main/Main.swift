2430c2f94be4d1fbdb4a9feb8b453ee2961d4a91
Alexandre Perrin <alex@kaworu.ch>
2017-12-13 21:31:47 +0100

    Day 5: A Maze of Twisty Trampolines, All Alike (part 2)

--- Day 05 - A Maze of Twisty Trampolines, All Alike/Sources/Main/Main.swift
+++ Day 05 - A Maze of Twisty Trampolines, All Alike/Sources/Main/Main.swift
@@ -11,4 +11,5 @@ func get_puzzle() -> [String] {
 
 let puzzle = get_puzzle().map { Int($0)! }
 let maze   = TwistyTrampolinesMaze(puzzle)
-print("THe exit is reached in \(maze.exit) steps.")
+print("The exit is reached in \(maze.strange_jumps_exit) steps,")
+print("and \(maze.stranger_jumps_exit) with the new rules.")
--- Day 05 - A Maze of Twisty Trampolines, All Alike/Sources/TwistyTrampolinesMaze/TwistyTrampolinesMaze.swift
+++ Day 05 - A Maze of Twisty Trampolines, All Alike/Sources/TwistyTrampolinesMaze/TwistyTrampolinesMaze.swift
@@ -1,18 +1,31 @@
 public class TwistyTrampolinesMaze {
-  let trampoline: [Int]
+  let instructions: [Int]
 
-  public init(_ trampoline: [Int]) {
-    self.trampoline = trampoline
+  public init(_ instructions: [Int]) {
+    self.instructions = instructions
   }
 
   // NOTE: part one
-  public var exit: Int {
-    var (pc, step, trampoline) = (0, 0, self.trampoline)
-    while pc >= 0 && pc < trampoline.count {
-      step += 1
-      trampoline[pc] += 1
-      pc += trampoline[pc] - 1
+  public var strange_jumps_exit: Int {
+    return step_count_to_exit { $0 + 1 }
+  }
+
+  // NOTE: part two
+  public var stranger_jumps_exit: Int {
+    return step_count_to_exit { $0 + ($0 >= 3 ? -1 : +1) }
+  }
+
+  // follow the maze instructions using the provided `update' function to
+  // compute the new instruction value under `pc' after each jump. Returns the
+  // number of step performed before we reached an exit.
+  internal func step_count_to_exit(_ update: (Int) -> Int) -> Int {
+    var (pc, nstep, instructions) = (0, 0, self.instructions)
+    while pc >= 0 && pc < instructions.count {
+      let instruction = instructions[pc]
+      instructions[pc] = update(instruction)
+      pc += instruction
+      nstep += 1
     }
-    return step
+    return nstep
   }
 }
